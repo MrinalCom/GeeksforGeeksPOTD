76d4e563b875269440f39d8884db5f01de4e9c8b
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-03-30 22:52:29 +0530

    Create Minimuminteger.cpp

---
+++ Minimuminteger.cpp
@@ -0,0 +1,16 @@
+
+        long long  sum=0;
+        int min=INT_MAX;
+        for(int i=0;i<N;i++) {
+        sum+=A[i];
+        }
+    for(int i=0;i<N;i++) {
+        if(A[i]*(long)N>=sum) {
+        if(A[i]<min) {
+            min=A[i];
+        }
+        }
+    }
+    return (int)min;
+    
+    }
