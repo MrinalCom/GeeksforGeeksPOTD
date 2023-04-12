aad7f31d6867aeec81bbb86d8b6b88e72397353b
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-04-12 19:40:29 +0530

    Update DominantPairs.cpp

--- DominantPairs.cpp
+++ DominantPairs.cpp
@@ -1,10 +1,18 @@
-int count=0;
-        for(int i=0;i<n/2;i++){
-            for(int j=n/2;j<n;j++){
-                if(arr[i]>=5*arr[j])
-                    count++;
-                else
-                    continue;
+sort(arr.begin(), arr.begin()+n/2);
+        sort(arr.begin()+n/2, arr.end());
+        
+        for(int j=n/2;j<n;j++) arr[j]=arr[j]*5;
+        
+        int i=0 ,j= n/2;
+        int ans=0;
+        
+        while(i< n/2 and j<n ){
+            if(arr[i]>= arr[j]){
+                ans+=n/2-i;
+                j++;
             }
+            else
+            i++;
         }
-        return count;
+        return ans;
+        
