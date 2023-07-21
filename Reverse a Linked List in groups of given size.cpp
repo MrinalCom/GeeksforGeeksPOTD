8f2005894abbfd0ae61d41be84ad9a8520eeae4f
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-07-21 21:13:36 +0530

    Create Reverse a Linked List in groups of given size.cpp

---
+++ Reverse a Linked List in groups of given size.cpp
@@ -0,0 +1,17 @@
+ if(head == NULL)
+            return NULL;
+        node* next = NULL;  
+        node* curr = head;
+        node* prev = NULL;
+        int count = 0;
+        while(curr != NULL && count<k){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+            count++;
+        }
+        if(next !=NULL){
+            head->next = reverse(next,k);
+        }
+        return prev;
