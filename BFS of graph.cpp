1358af8ee393aceb4ebb2bae91cc279c4bf7d5a2
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-07-31 15:41:51 +0530

    Create BFS of graph.cpp

---
+++ BFS of graph.cpp
@@ -0,0 +1,17 @@
+vector<int>res;
+        queue<int>q;
+        q.push(0);
+        map<int,int>vis;
+        vis[0]=1;
+        while(!q.empty()){
+            int topp=q.front();
+            q.pop();
+            res.push_back(topp);
+            for(auto child:adj[topp]){
+                if(!vis[child]){
+                    vis[child]=1;
+                    q.push(child);
+                }
+            }
+        }
+        return res;
