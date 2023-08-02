d3d81762ae2e61a8c648f8e66249adc0abff2176
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-08-03 01:37:42 +0530

    Create Shortest path in Directed Acyclic Graph.cpp

---
+++ Shortest path in Directed Acyclic Graph.cpp
@@ -0,0 +1,16 @@
+vector<int>dist(n, 1e8);
+        dist[0] = 0;
+        
+        for(int i=0; i<m; i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int cost = edges[i][2];
+            if(dist[v]>cost+dist[u])
+                dist[v] = cost+dist[u];
+        }
+        
+        for(int val=0; val<dist.size(); val++)
+            if(dist[val]==1e8)
+                dist[val] = -1;
+                
+        return dist;
