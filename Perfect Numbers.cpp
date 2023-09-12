ef0f7d702848d561753c62fb94d2c7744141b9fb
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-09-12 11:02:41 +0530

    Create Perfect Numbers.cpp

---
+++ Perfect Numbers.cpp
@@ -0,0 +1,15 @@
+ isPerfectNumber(long long N) {
+
+    if (N <= 1) {
+        return 0; 
+    }
+    long long sum = 1;
+    for (long long i = 2; i * i <= N; i++) {
+        if (N % i == 0) {
+            sum += i; 
+            if (i * i != N) {
+                sum += N / i; 
+            }
+        }
+    }
+    return sum == N;
