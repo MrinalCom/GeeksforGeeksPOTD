f559d373277dd0ef117a4bcdcbdee4695c433364
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-11-08 10:57:55 +0530

    Create Print Matrix in snake Pattern.cpp

---
+++ Print Matrix in snake Pattern.cpp
@@ -0,0 +1,16 @@
+ vector<int>ans;
+        int row=matrix.size();
+        int col=matrix[0].size();
+        for(int i=0;i<row;i++){
+            if(i%2==0){
+                 for(int j=0;j<col;j++){
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+            else{
+                for(int j=col-1;j>=0;j--){
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+        }
+        return ans;
