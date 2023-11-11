54f64538899e2663b072a30c090143e3e4bb3a3d
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-11-11 14:32:48 +0530

    Create Isomorphic Strings.cpp

---
+++ Isomorphic Strings.cpp
@@ -0,0 +1,18 @@
+ if(str1.size() != str2.size())
+            return false;
+        unordered_map<char, char> m1, m2;
+        for(int i = 0; i < str1.size(); i++) {
+            if(m1[str1[i]] && m1[str1[i]] != str2[i]) {
+                    return false;
+            }
+            else {
+                m1[str1[i]] = str2[i];
+            }
+            if(m2[str2[i]] && m2[str2[i]] != str1[i]) {
+                    return false;
+            }
+            else {
+                m2[str2[i]] = str1[i];
+            }
+        }
+        return true;
