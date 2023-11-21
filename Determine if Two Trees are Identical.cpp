286af840f7085b8b70d2c1fabea6b64557b8925c
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-11-21 19:17:42 +0530

    Create Determine if Two Trees are Identical.cpp

---
+++ Determine if Two Trees are Identical.cpp
@@ -0,0 +1,10 @@
+if(r1==NULL && r2==NULL){
+            return true;
+        }
+        if(r1 !=NULL && r2!=NULL){
+            if(r1->data==r2->data){
+                return isIdentical(r1->left , r2->left)&&isIdentical(r1->right , r2->right);
+            }
+        }
+        
+        return false;
