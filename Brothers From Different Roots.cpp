02f1f90e5722a55ec7ac803d7c80d527fc03ce78
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-12-03 19:13:57 +0530

    Create Brothers From Different Roots.cpp

---
+++ Brothers From Different Roots.cpp
@@ -0,0 +1,28 @@
+void inorder(Node* root, vector<int>&v)
+    {
+        if(root==NULL) return;
+        inorder(root->left,v);
+        v.push_back(root->data);
+        inorder(root->right,v);
+    }
+    int countPairs(Node* root1, Node* root2, int x)
+    {
+        vector<int>v1,v2;
+        inorder(root1,v1);
+        inorder(root2,v2);
+        
+        int i=0;
+        int j=v2.size()-1;
+        
+        int c=0;
+        while(i<v1.size() and j>=0)
+        {
+            if(v1[i]+v2[j]==x)
+            {
+                c++;
+                i++;
+                j--;
+            }else if(v1[i]+v2[j]>x) j--;
+            else i++;
+        }
+        return c;
