a2bf2efea7bf4b5d2ea7acf98e1a7cd22bf42dde
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-12-15 17:57:15 +0530

    Create Reach the Nth point.cpp

---
+++ Reach the Nth point.cpp
@@ -0,0 +1,10 @@
+int f1 = 1, f2 = 1;
+		    int f = 0;
+		    while(n){
+		        f = f1 + f2;
+		        f = f%1000000007;
+		        f1 = f2;
+		        f2 = f;
+		        n--;
+		    }
+		    return f1;
