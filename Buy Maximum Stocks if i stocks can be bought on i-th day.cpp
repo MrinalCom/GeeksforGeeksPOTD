19df7a0dfd7ce864b9f0aef52c80c3ef103f3bd3
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2023-12-24 00:49:58 +0530

    Create Buy Maximum Stocks if i stocks can be bought on i-th day.cpp

---
+++ Buy Maximum Stocks if i stocks can be bought on i-th day.cpp
@@ -0,0 +1,16 @@
+vector<pair<int, int>> v;
+        int cnt=0, tmp;
+        for(int i=0; i<n; i++){
+            v.push_back(make_pair(price[i], i));
+        }
+        sort(v.begin(), v.end());
+        for(int i=0; i<n; i++){
+            tmp=v[i].second;
+            while(tmp>=0){
+                if(k<v[i].first) return cnt;
+                k-=v[i].first;
+                tmp--;
+                cnt++;
+            }
+        }
+        return cnt;
