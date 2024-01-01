a8941ce4e45c374f134002036929496b0e2445b6
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2024-01-01 15:13:53 +0530

    Create Array Pair Sum Divisibility Problem.cpp

---
+++ Array Pair Sum Divisibility Problem.cpp
@@ -0,0 +1,14 @@
+int n=nums.size();
+         if(n%2) return false;
+         unordered_map<int,int>mp;
+         for(int i=0;i<n;i++){
+             mp[nums[i]%k]++;
+         }
+         for(auto it:mp){
+             int p=it.first;
+             int q=it.second;
+             int req=k-p;
+             if(p==0 and mp[p]%2==0) continue;
+             if(mp[req]!=q) return 0;             
+         }
+         return 1;
