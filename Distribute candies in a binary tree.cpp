86a36e0410e9bf137d3322bae5c95e5a4c000e8a
Mrinal Anand <97303675+MrinalCom@users.noreply.github.com>
2024-01-20 22:45:01 +0530

    Create Distribute candies in a binary tree.cpp

---
+++ Distribute candies in a binary tree.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    public:
+    int solve(Node *root, int &ans)
+    {
+        if(root==NULL)
+        {
+            return 0;
+        }
+        
+        int left=solve(root->left, ans);
+        int right=solve(root->right, ans);
+        
+        ans+=abs(left)+abs(right);
+        int extraCandies = root->key + left + right - 1;
+        return extraCandies;
+    }
+    int distributeCandy(Node* root)
+    {
+        //code here
+        int ans=0;
+        solve(root,ans);
+        return ans;
+    }
+};
